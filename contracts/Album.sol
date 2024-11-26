3106580cbbc826f30dadd5d82382f67eb9738f13
Ricardo FRanco Hernandez <ricardo.hern@icloud.com>
2024-11-26 19:38:44 -0300

    progress

--- contracts/Album.sol
+++ contracts/Album.sol
@@ -8,6 +8,8 @@ contract Album {
 
     string public constant contractAlbum = 'Kris Bennet';
 
+    address public owner;
+
     // Event which will be raised anytime the current album information is updated
     event albumEvent(string albumEvent_Artist, string albumEvent_Title, uint albumEvent_Tracks);
 
@@ -15,18 +17,25 @@ contract Album {
         artist = "Nirvana";
         albumTitle = "Nevermind";
         tracks = 13;
+
+        owner = msg.sender;
     }
 
     function getAlbum() public view returns(string memory, string memory, uint) {
         return (artist, albumTitle, tracks);
     }
 
-    function setAlbum(string memory _artist, string memory _albumTitle, uint _tracks) public {
+    function setAlbum(string memory _artist, string memory _albumTitle, uint _tracks) onlyOwner public {
         artist = _artist;
         albumTitle = _albumTitle;
         tracks = _tracks;
 
         // Raise the algum event to let any event subscribers know the current algum informaition has changed.
         emit albumEvent(_artist, _albumTitle, _tracks);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 }
