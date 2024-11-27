a45480a05e5e1648188341cfb36813df962cce32
Ricardo FRanco Hernandez <ricardo.hern@icloud.com>
2024-11-27 11:22:24 -0300

    progress

--- contracts/Album.sol
+++ contracts/Album.sol
@@ -2,40 +2,99 @@
 pragma solidity ^0.8.24;
 
 contract Album {
-    string public artist;
-    string public albumTitle = "Nevermind";
-    uint public tracks;
+    struct musicAlgum {
+        string artist;
+        string albumTitle;
+        uint tracks;
+    }
+    musicAlgum public currentAlbum;
+    mapping(address => musicAlgum) public userAlbuns;
 
-    string public constant contractAlbum = 'Kris Bennet';
-
+    string public constant contractAlbum = "Kris Bennet";
     address public owner;
 
     // Event which will be raised anytime the current album information is updated
-    event albumEvent(string albumEvent_Artist, string albumEvent_Title, uint albumEvent_Tracks);
+    event albumEvent(
+        string algumEvent_Description,
+        string albumEvent_Artist,
+        string albumEvent_Title,
+        uint albumEvent_Tracks
+    );
+
+    event errorEvent(string errorEvent_Message);
 
     constructor() {
-        artist = "Nirvana";
-        albumTitle = "Nevermind";
-        tracks = 13;
+        currentAlbum.artist = "Nirvana";
+        currentAlbum.albumTitle = "Nevermind";
+        currentAlbum.tracks = 13;
 
         owner = msg.sender;
     }
 
-    function getAlbum() public view returns(string memory, string memory, uint) {
-        return (artist, albumTitle, tracks);
+    function getCurrentlbum()
+        public
+        view
+        returns (string memory, string memory, uint)
+    {
+        return (
+            currentAlbum.artist,
+            currentAlbum.albumTitle,
+            currentAlbum.tracks
+        );
     }
 
-    function setAlbum(string memory _artist, string memory _albumTitle, uint _tracks) onlyOwner public {
-        artist = _artist;
-        albumTitle = _albumTitle;
-        tracks = _tracks;
+    function setCurrentAlbum(
+        string memory _artist,
+        string memory _albumTitle,
+        uint _tracks
+    ) public onlyOwner {
+        currentAlbum.artist = _artist;
+        currentAlbum.albumTitle = _albumTitle;
+        currentAlbum.tracks = _tracks;
 
         // Raise the algum event to let any event subscribers know the current algum informaition has changed.
-        emit albumEvent(_artist, _albumTitle, _tracks);
+        emit albumEvent(
+            "The current album information has been updated",
+            _artist,
+            _albumTitle,
+            _tracks
+        );
+    }
+
+    function getUserFavoriteAlbum()
+        public
+        view
+        returns (string memory, string memory, uint)
+    {
+        return (
+            userAlbuns[msg.sender].artist,
+            userAlbuns[msg.sender].albumTitle,
+            userAlbuns[msg.sender].tracks
+        );
+    }
+
+    function setUsersFavoriteAlbum(
+        string memory _artist,
+        string memory _albumTitle,
+        uint _tracks
+    ) public {
+        userAlbuns[msg.sender].artist = _artist;
+        userAlbuns[msg.sender].albumTitle = _albumTitle;
+        userAlbuns[msg.sender].tracks = _tracks;
+
+        emit albumEvent(
+            "You have updated your personal favorite album information",
+            _artist,
+            _albumTitle,
+            _tracks
+        );
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+        if (msg.sender == owner) {
+            _;
+        } else {
+            emit errorEvent("Only the owner can call this function");
+        }
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/Fmyf_pl7WZemB2FNg_wLl6Hza47tZpAW",
+      accounts: [process.env.PRIVATE_KEY1],
+      gas: 3000000, // Gas limit
+      gasPrice: 30000000000, // 30 Gwei
+    }
+  },
 };
